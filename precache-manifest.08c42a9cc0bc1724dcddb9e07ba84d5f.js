913a343c1d4fe419bc1467736efeb257dc388de3
vaishakh kp <kpvaisakh@gmail.com>
2019-05-28 12:52:05 +0530

    Updates

---
+++ precache-manifest.08c42a9cc0bc1724dcddb9e07ba84d5f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a997f257a710abfc7a1eaea7b763ce67",
+    "url": "/portfolio-one/index.html"
+  },
+  {
+    "revision": "a9235ab2f4a42df96e81",
+    "url": "/portfolio-one/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "c1144e4b2a872e2bb09c",
+    "url": "/portfolio-one/static/css/main.2ac389a1.chunk.css"
+  },
+  {
+    "revision": "a9235ab2f4a42df96e81",
+    "url": "/portfolio-one/static/js/2.7cd6201c.chunk.js"
+  },
+  {
+    "revision": "c1144e4b2a872e2bb09c",
+    "url": "/portfolio-one/static/js/main.e322b0d9.chunk.js"
+  },
+  {
+    "revision": "96347398786dcdd7e946",
+    "url": "/portfolio-one/static/js/runtime~main.2c50c429.js"
+  },
+  {
+    "revision": "7ad1e7dd4ece272678a701772eab9e46",
+    "url": "/portfolio-one/static/media/avatar-2.7ad1e7dd.jpeg"
+  },
+  {
+    "revision": "9ce81cdc80d98412ebdeab458dd63846",
+    "url": "/portfolio-one/static/media/avatar-3.9ce81cdc.jpeg"
+  },
+  {
+    "revision": "ecdec30003b7bb2ab988d90e793fe142",
+    "url": "/portfolio-one/static/media/avatar-4.ecdec300.jpeg"
+  }
+]);
